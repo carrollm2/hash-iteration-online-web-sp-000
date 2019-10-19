451aa0e3dcbe373d70e45ef930b2f85b19ffc5cb
Michael Carroll <carrollm2@gmail.com>
2019-10-19 19:37:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,7 +10,8 @@ def select_winner(passengers)
 
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      return kids_name
+      winner = name
     end
   end
+  winner
 end
