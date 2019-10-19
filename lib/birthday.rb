111e32fa3430e813d5d7464b3b31d374cf7830f9
Michael Carroll <carrollm2@gmail.com>
2019-10-19 19:34:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,12 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+
+def select_winner(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if kids_name.start_with?("A")
+      return kids_name
+    end
+  end
+end
